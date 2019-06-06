5053768b4dc5ddb7141c830a2dd7b6f0a79dfb55
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-06-06 14:09:04 +0200

    Created AddTaskActivity activity and layout.

---
+++ app/src/main/java/com/krolseb/todolistapp/AddTaskActivity.java
@@ -0,0 +1,13 @@
+package com.krolseb.todolistapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTaskActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
